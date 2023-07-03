84f0b14ea632bae1a668e0669266dcdbf94137e7
shashank <shashanksinghania120@gmail.com>
2023-07-03 19:03:31 +0530

    update

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,21 +1,15 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   // apiKey: process.env.REACT_APP_KEY,
-  apiKey:"AIzaSyC22fm4wJVhuRrfmRfE8xpTYryOvVv1MZM",
-  authDomain: "whatsapp-clone-83bf0.firebaseapp.com",
-  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: "https://whatsapp-clone-83bf0-default-rtdb.firebaseio.com",
-  // databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: "whatsapp-clone-83bf0",
-  // projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: "whatsapp-clone-83bf0.appspot.com",
-  // storageBucket: process.env.REACT_APP_STOREAGE_BUCKET,
-  messagingSenderId: "347059772726",
-  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: "1:347059772726:web:bf6c6904a52275be75e797",
-  // appId: process.env.REACT_APP_APP_ID,
-  measurementId: "G-RSMDX8CD9G",
-  // measurementId: process.env.REACT_APP_MEASURMENT_ID,
+
+
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STOREAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASURMENT_ID,
 };
 
 export default firebaseConfig;
